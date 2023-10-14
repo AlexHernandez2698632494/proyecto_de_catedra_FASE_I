3a3bbb258043945532c3547c13527cbeaa77b527
DanielMenjivar20 <dani20.menjivar@gmail.com>
2023-10-13 21:38:04 -0600

    falta el modal

--- js/obrasCompras.js
+++ js/obrasCompras.js
@@ -1,3 +1,11 @@
-const openModal= document.querySelector("abrir_cta");
-const modal= document.querySelector(".modal");
+const abrir_cta = document.getElementById('abrir_cta');
+const modal_container = document.getElementById('modal_container');
+const modal_close = document.getElementById('modal_close');
 
+abrir_cta.addEventListener('click', () => {
+  modal_container.classList.add('show');  
+});
+
+modal_close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
